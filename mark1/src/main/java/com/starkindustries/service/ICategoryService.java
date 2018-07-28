78f2e9fe65c02712b67f9904079f35791cb58760
Jarvis <zheng.zhao.nj@gmail.com>
2018-07-28 18:30:27 +0800

    添加分类接口

---
+++ mark1/src/main/java/com/starkindustries/service/ICategoryService.java
@@ -0,0 +1,10 @@
+package com.starkindustries.service;
+
+import com.starkindustries.common.ServerResponse;
+
+public interface ICategoryService {
+
+    ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+}
